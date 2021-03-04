d4bd4541934b62bb5d8b121e1be4f6866a5e2672
yassentarek <yassen.tarek23@gmail.com>
2021-03-05 01:19:17 +0200

    -

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,12 +14,17 @@
 # end
 #
 
-fish_vi_key_bindings
+fish_vi_key_bindings			# the fish key mode - here is set to vim - 	
+set fish_cursor_replace_one line blink  # set the cursor shape to line
+set fish_cursor_insert line blink	# set the cursor shape to line
+set fish_cursor_default line blink	# set the cursor shape to line
+set fish_cursor_visual line blink	# set the cursor shape to line
+set fish_greeting			# disable the fish greeting
 
-set fish_greeting
-
+### some aliases
 alias rm='trash'
 alias ..="cd .."
-colorscript random
-starship init fish | source
+
+colorscript random			# trigger the color welcome script
+starship init fish | source		# trigger the starship prombt
 
