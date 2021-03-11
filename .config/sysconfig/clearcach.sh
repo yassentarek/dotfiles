c322637c600c999f74aa8fd930d4943d47bbf68b
yassentarek <yassen.tarek23@gmail.com>
2021-03-11 21:12:48 +0200

    -

--- .config/sysconfig/clearcach.sh
+++ .config/sysconfig/clearcach.sh
@@ -2,8 +2,7 @@
 
 # script to free up some ram run it with cron
 
-free -h > /home/yassen/Desktop/free-befor-clear
+free -h > /home/yassen/Desktop/befor
 sync; echo 1 > /proc/sys/vm/drop_caches
 sync; echo 2 > /proc/sys/vm/drop_caches
-sync; echo 3 > /proc/sys/vm/drop_caches
-free -h > /home/yassen/Desktop/free-after-clear
+free -h > /home/yassen/Desktop/after
