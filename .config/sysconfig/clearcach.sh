11c769939c1aa770dca03336a85afc964e95e32c
yassentarek <yassen.tarek23@gmail.com>
2021-03-13 21:59:18 +0200

    -

--- .config/sysconfig/clearcach.sh
+++ .config/sysconfig/clearcach.sh
@@ -3,6 +3,8 @@
 # script to free up some ram run it with cron
 
 free -h > /home/yassen/Desktop/befor
+
 sync; echo 1 > /proc/sys/vm/drop_caches
 sync; echo 2 > /proc/sys/vm/drop_caches
+
 free -h > /home/yassen/Desktop/after
---
+++ .config/sysconfig/rename.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+# this script rename files by numbers 001 002 003 etc...
+# to add more zeors change the "%04d" to whatever zeros wanter
+#rename 's/\d+/sprintf("%04d", $&)/e' *.jpg 
