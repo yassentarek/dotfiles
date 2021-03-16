af5265e3cd63d477e39195112738352717b0a0f4
yassentarek <yassen.tarek23@gmail.com>
2021-03-16 14:56:03 +0200

    -

---
+++ packages.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#needs to be reviwed befoe install
+
+sudo apt install python2 python3 python3-pyxattr python3-pip vim neovim xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings nitrogen arandr alacritty picom dmenu rofi firefox  htop youtube-dl git ffmpeg pcmanfm mpv fish sxiv nomacs ncdu ranger fonts-font-awesome fonts-powerline trash-cli tldr zathura xreader 
