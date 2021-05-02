2deae80ece68cfa1d80555ecbf085befb79bc882
yassentarek <yassen.tarek23@gmail.com>
2021-05-03 01:34:20 +0200

    -

--- packages.sh
+++ packages.sh
@@ -8,12 +8,13 @@ sudo apt install python3 python3-pip vim neovim dmenu rofi firefox  htop youtube
 sudo git clone https://gitlab.com/yassen.tarek23/shell-color-scripts.git
 #download starship prombet 
 curl https://raw.githubusercontent.com/anhsirk0/fetch-master-6000/master/fm6000.pl --output fm6000
-#chmod +x the fm6000 file 
+#chmod +x the fm6000 file and place it into .config
 curl -fsSL https://starship.rs/install.sh | bash
-#download icon theme 
+# place it into .config
 sudo add-apt-repository ppa:papirus/papirus
 sudo apt-get update
 sudo apt-get install papirus-icon-theme
+#download icon theme 
 # install some arabic fonts use this links 
 # https://arbfonts.com/noto-sans-arabic-bold-font-download.html
 # https://arbfonts.com/noto-naskh-arabic-ui-font-download.html 
