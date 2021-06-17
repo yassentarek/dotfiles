e8bb0083d3f289aa4753a936c0b6f8922e0487c9
yassentarek <yassen.tarek23@gmail.com>
2021-06-17 02:04:28 +0200

    -

--- .config/sysconfig/clearcach.sh
+++ .config/sysconfig/clearcach.sh
@@ -7,6 +7,6 @@ free -h > /home/yassen/Desktop/befor
 sync; echo 1 > /proc/sys/vm/drop_caches
 sync; echo 2 > /proc/sys/vm/drop_caches
 
-free -h > /home/yassen/Desktop/after
-echo " " >> /home/yassen/Desktop/after
-date >> /home/yassen/Desktop/after
+#free -h > /home/yassen/Desktop/after
+#echo " " >> /home/yassen/Desktop/after
+#date >> /home/yassen/Desktop/after
