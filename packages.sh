428c4f663b3574685422e6d3c34351965b50694e
yassentarek <yassen.tarek23@gmail.com>
2021-07-11 01:20:06 +0200

    -

--- packages.sh
+++ packages.sh
@@ -3,7 +3,7 @@
 ############################ this script is for debian based ###################################
 
 #download main packages that i can't live without it
-sudo apt install python3 python3-pip vim neovim dmenu rofi firefox  htop youtube-dl git ffmpeg pcmanfm mpv fish sxiv nomacs ncdu ranger fonts-font-awesome fonts-powerline trash-cli tldr zathura shellcheck
+sudo apt install python3 python3-pip vim neovim dmenu rofi firefox  htop youtube-dl git ffmpeg pcmanfm mpv fish sxiv nomacs ncdu ranger fonts-font-awesome fonts-powerline trash-cli tldr zathura shellcheck 
 #download the colorscript repo - installation is in https://gitlab.com/dwt1/shell-color-scripts.git
 sudo git clone https://gitlab.com/yassen.tarek23/shell-color-scripts.git
 #download starship prombet 
@@ -18,3 +18,4 @@ sudo apt-get install papirus-icon-theme
 # install some arabic fonts use this links 
 # https://arbfonts.com/noto-sans-arabic-bold-font-download.html
 # https://arbfonts.com/noto-naskh-arabic-ui-font-download.html 
+#download imagemagice and move the policy.xml to /etc/imagemagic.. to edit the original policy it enables imagemagic to make pdf file from multible images 
