5f49c7ac2e9ec00245a684d401865a0055f0cdc7
yassentarek <yassen.tarek23@gmail.com>
2021-10-11 01:23:46 +0200

    -

--- packages.sh
+++ packages.sh
@@ -3,7 +3,7 @@
 ############################ this script is for debian based ###################################
 
 #download main packages that i can't live without it
-sudo apt install python3 python3-pip vim neovim dmenu rofi firefox  htop youtube-dl git ffmpeg pcmanfm mpv fish sxiv nomacs ncdu ranger fonts-font-awesome fonts-powerline trash-cli tldr zathura shellcheck hardinfo
+sudo apt install python3 python3-pip vim neovim dmenu rofi firefox  htop youtube-dl git ffmpeg pcmanfm mpv fish sxiv nomacs ncdu ranger fonts-font-awesome fonts-powerline trash-cli tldr zathura shellcheck hardinfo ical xmind
 #download the colorscript repo - installation is in https://gitlab.com/dwt1/shell-color-scripts.git
 sudo git clone https://gitlab.com/yassen.tarek23/shell-color-scripts.git
 #download starship prombet 
